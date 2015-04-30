a4fa77f7162f3bb54805070a9130f47e6bd20cd9
Kofei <aidingyue@gmail.com>
2015-04-30 11:59:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 	
 	// set up ========================
 	var express  = require('express');
-	var app      = express();                     // create our app w/ express
-	var mongoose = require('mongoose'); 					// mongoose for mongodb
+	var app      = express();                       // create our app w/ express
+	var mongoose = require('mongoose'); 		// mongoose for mongodb
 	var port	 = process.env.PORT || 8080;
-	var database = require('./config/database');  // load the config
+	var database = require('./config/database');    // load the config
 
 	// configuration =================
 
@@ -13,9 +13,9 @@
 
 	app.configure(function() {
 		app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-		app.use(express.logger('dev')); 					        	// log every request to the console
-		app.use(express.bodyParser()); 							        // pull information from html in POST
-		app.use(express.methodOverride()); 					        // simulate DELETE and PUT
+		app.use(express.logger('dev')); 		        	// log every request to the console
+		app.use(express.bodyParser());                                  // pull information from html in POST
+		app.use(express.methodOverride()); 		                // simulate DELETE and PUT
 	});
 
 
